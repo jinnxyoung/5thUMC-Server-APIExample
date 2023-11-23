dc7f49ee51280146aaeb53133cb9763aa3c93bdf
jinnxyoung <wlsdud7722@naver.com>
2023-11-23 17:35:12 +0900

    Feat: package 구조 설정 및 API 응답 통일 구현

---
+++ src/main/java/com/umc/spring/apiPayload/code/BaseCode.java
@@ -0,0 +1,6 @@
+package com.umc.spring.apiPayload.code;
+
+public interface BaseCode {
+    public ReasonDTO getReason();
+    public ReasonDTO getReasonHttpStatus();
+}
---
+++ src/main/java/com/umc/spring/apiPayload/code/BaseErrorCode.java
@@ -0,0 +1,6 @@
+package com.umc.spring.apiPayload.code;
+
+public interface BaseErrorCode {
+    public ErrorReasonDTO getReason();
+    public ErrorReasonDTO getReasonHttpStatus();
+}
