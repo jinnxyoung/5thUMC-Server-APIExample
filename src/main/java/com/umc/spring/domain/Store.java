a2404691079f387fc2d0b7d053a2a1ec3802cd64
jinnxyoung <wlsdud7722@naver.com>
2023-11-29 18:10:34 +0900

    Feat: domain Store 구현

---
+++ src/main/java/com/umc/spring/domain/Store.java
@@ -0,0 +1,35 @@
+package com.umc.spring.domain;
+
+import lombok.*;
+import com.umc.spring.domain.common.BaseEntity;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Store extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String address;
+
+    private Float score;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "region_id")
+    private Region region;
+
+    @OneToMany(mappedBy = "store", cascade = CascadeType.ALL)
+    private List<Mission> missionList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "store", cascade = CascadeType.ALL)
+    private List<Review> reviewList = new ArrayList<>();
+}
