a1edcbda4a1570455e196a24279a3a7e17e5c5b4
jinnxyoung <wlsdud7722@naver.com>
2023-11-29 18:10:19 +0900

    Feat: domain Region 구현

---
+++ src/main/java/com/umc/spring/domain/Region.java
@@ -0,0 +1,25 @@
+package com.umc.spring.domain;
+
+import lombok.*;
+import lombok.extern.java.Log;
+import com.umc.spring.domain.common.BaseEntity;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Region extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "region", cascade = CascadeType.ALL)
+    private List<Store> storeList = new ArrayList<>();
+}
