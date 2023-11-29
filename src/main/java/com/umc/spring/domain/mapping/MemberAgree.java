da1071c481de37ecf7f0f3b447dd302c79869ea8
jinnxyoung <wlsdud7722@naver.com>
2023-11-29 18:07:10 +0900

    Feat: mapping MemberAgree 구현

---
+++ src/main/java/com/umc/spring/domain/mapping/MemberAgree.java
@@ -0,0 +1,28 @@
+package com.umc.spring.domain.mapping;
+
+import lombok.*;
+
+import com.umc.spring.domain.Member;
+import com.umc.spring.domain.Terms;
+import com.umc.spring.domain.common.BaseEntity;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class MemberAgree extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "terms_id")
+    private Terms terms;
+}
