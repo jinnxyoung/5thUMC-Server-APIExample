e77e011c73047e99a3f18f00ad78ae0212614f96
jinnxyoung <wlsdud7722@naver.com>
2023-11-29 18:10:45 +0900

    Feat: domain Terms 구현

---
+++ src/main/java/com/umc/spring/domain/Terms.java
@@ -0,0 +1,29 @@
+package com.umc.spring.domain;
+
+import lombok.*;
+import com.umc.spring.domain.common.BaseEntity;
+import com.umc.spring.domain.mapping.MemberAgree;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Terms extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String body;
+
+    private Boolean optional;
+
+    @OneToMany(mappedBy = "terms", cascade = CascadeType.ALL)
+    private List<MemberAgree> memberAgreeList = new ArrayList<>();
+}
