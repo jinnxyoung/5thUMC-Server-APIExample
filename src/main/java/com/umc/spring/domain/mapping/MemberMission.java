bffd72c2641eccdf7136e74791dec1cb677a387b
jinnxyoung <wlsdud7722@naver.com>
2023-11-29 18:07:23 +0900

    Feat: mapping MemberMission 구현

---
+++ src/main/java/com/umc/spring/domain/mapping/MemberMission.java
@@ -0,0 +1,31 @@
+package com.umc.spring.domain.mapping;
+
+import lombok.*;
+import com.umc.spring.domain.Member;
+import com.umc.spring.domain.Mission;
+import com.umc.spring.domain.common.BaseEntity;
+import com.umc.spring.domain.enums.MissionStatus;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class MemberMission extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private MissionStatus status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "mission_id")
+    private Mission mission;
+}
