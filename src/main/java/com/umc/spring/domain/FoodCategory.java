1aec40fed19256f8ae8f2cacf75af67dba8ccf2c
jinnxyoung <wlsdud7722@naver.com>
2023-11-29 18:09:46 +0900

    Feat: domain FoodCategory 구현

---
+++ src/main/java/com/umc/spring/domain/FoodCategory.java
@@ -0,0 +1,22 @@
+package com.umc.spring.domain;
+
+import lombok.*;
+import com.umc.spring.domain.common.BaseEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class FoodCategory extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
